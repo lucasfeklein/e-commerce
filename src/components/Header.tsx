2a782bbb36e399b98718ea21b478ce7a58af6d71
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-18 14:27:49 -0300

    text header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,30 @@
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-6">
-      <div className="flex gap-7">
-        <img className="mr-4" src="./logo.svg" />
-        <p>Collection</p>
-        <p>Men</p>
-        <p>About</p>
-        <p>Contact</p>
+    <div className="flex justify-between items-center py-6 border-b">
+      <div className="flex gap-7 ">
+        <div>
+          <img className="mr-4" src="./logo.svg" />
+        </div>
+        <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800">
+          Collection
+        </p>
+        <p className="cursor-pointer text-gray-500 hover:text-gray-800">Men</p>
+        <p className="cursor-pointer text-gray-500 hover:text-gray-800">
+          About
+        </p>
+        <p className="cursor-pointer text-gray-500 hover:text-gray-800">
+          Contact
+        </p>
       </div>
       <div className="flex gap-7 items-center">
         <div>
-          <img width={25} src="./icon-cart.svg" />
+          <img width={25} src="./icon-cart.svg" className="cursor-pointer" />
         </div>
-        <img width={40} src="./image-avatar.png" />
+        <img
+          className="border-orange-500 rounded-full border-2 cursor-pointer"
+          width={40}
+          src="./image-avatar.png"
+        />
       </div>
     </div>
   );
