4a8005fddc0ff4497d2f1618d9eca6f65f75edea
lucasfeklein <lucas.fklein@hotmail.com>
2023-12-18 14:42:46 -0300

    hover margin bottom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,24 @@
 const Header = () => {
   return (
-    <div className="flex justify-between items-center py-6 border-b">
-      <div className="flex gap-7 ">
+    <div className="flex justify-between border-b">
+      <div className="flex gap-7 mt-[40px]">
         <div>
           <img className="mr-4" src="./logo.svg" />
         </div>
-        <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800">
-          Collection
-        </p>
-        <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800">
-          Men
-        </p>
-        <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800">
-          About
-        </p>
-        <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800">
-          Contact
-        </p>
+        <div className="flex gap-6 h-[60px]">
+          <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800 hover:border-b-2 hover:border-b-orange-500">
+            Collection
+          </p>
+          <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800 hover:border-b-2 hover:border-b-orange-500">
+            Men
+          </p>
+          <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800 hover:border-b-2 hover:border-b-orange-500">
+            About
+          </p>
+          <p className="cursor-pointer text-gray-500 font-semibold hover:text-gray-800 hover:border-b-2 hover:border-b-orange-500">
+            Contact
+          </p>
+        </div>
       </div>
       <div className="flex gap-7 items-center">
         <div>
